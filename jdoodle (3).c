ca77950254c2d12d78d341561ed1b112fcd22940
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2019-08-19 14:47:17 +0530

    Add files via upload

---
+++ jdoodle (3).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int n,a,b,temp=0,i,f=0;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    scanf("%d %d",&a,&b);
+    temp=temp-a+b;
+    if(i==1)
+    {
+        f=temp;
+    }
+    if(f<=temp)
+    {
+        f=temp;
+    }
+  }
+  printf("%d",f);
+  return 0;
+}
