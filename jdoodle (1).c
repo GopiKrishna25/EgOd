3d58868115491dd6edd160f613cf50efa37d4155
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2019-08-19 12:20:32 +0530

    Add files via upload

--- jdoodle (1).c
+++ jdoodle (1).c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 
-int main() {
-    int i,j,k,rows;
-    
-    
-    
-    printf("enter no of rows: \n");
-    scanf("%d\n",&rows);
-    
-    for(i=1; i<=rows; i++)
+int main()
+{
+    int n,a,b,c;
+    scanf("%d %d %d %d",&n,&a,&b,&c);
+    int i=0,j=0,temp=0,flag=0;
+    for(i=0; i<=n; i++)
     {
-        for(j=(rows-i); j>=0; j--)
+        for(j=0; j<=n; j++)
         {
-            printf(" ");
+            if(i*a+j*b<=n)
+            {
+                if((n-(i*a+j*b))%c==0)
+                {
+                    flag=i+j+(n-(i*a+j*b))/c;
+                    if(flag>temp && flag!=0)
+                    {
+                        temp=flag;
+                    }
+                }
+            }
         }
-        for(k=1;k<=(i+1);k++)
-        {
-            printf("#");
-        }
-        printf("\n");
     }
+    printf("%d",temp);
+    return 0;
 }
-
- 
