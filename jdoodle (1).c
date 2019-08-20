d4110622de4a901db1017087ed7ff7df431a06e9
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2019-08-20 14:38:17 +0530

    Add files via upload

--- jdoodle (1).c
+++ jdoodle (1).c
@@ -2,26 +2,13 @@
 
 int main()
 {
-    int n,a,b,c;
-    scanf("%d %d %d %d",&n,&a,&b,&c);
-    int i=0,j=0,temp=0,flag=0;
-    for(i=0; i<=n; i++)
-    {
-        for(j=0; j<=n; j++)
-        {
-            if(i*a+j*b<=n)
-            {
-                if((n-(i*a+j*b))%c==0)
-                {
-                    flag=i+j+(n-(i*a+j*b))/c;
-                    if(flag>temp && flag!=0)
-                    {
-                        temp=flag;
-                    }
-                }
-            }
-        }
-    }
-    printf("%d",temp);
+    int n,m,min,max,l,k,o;
+    scanf("%d %d",&n,&m);
+    max=(n-m)*(n-m+1)/2;
+    l=n%m;
+    k=n/m;
+    o=n/m+1;
+    min=(m-l)*(k-1)*(k)/2+l*o*(o-1)/2;
+    printf("%d %d\n",max,min);
     return 0;
 }
