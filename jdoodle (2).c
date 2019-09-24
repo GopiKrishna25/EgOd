03bd87feed9506c5c0708df0b904cb7c686d0a6e
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2019-09-24 14:29:18 +0530

    Add files via upload

--- jdoodle (2).c
+++ jdoodle (2).c
@@ -1,89 +1,48 @@
-#include <stdio.h>
-#include <string.h>
- 
+#include<stdio.h>
+#include<string.h>
+
 int main()
 {
-        char a[205],ans[200];
-        int i,j=0,n=0;
-        scanf("%s",a);
- 
-        for(i=0;i<strlen(a);i++)
+    int i;
+    char a[100];
+    scanf("%s",a);
+    
+    
+    for(i=0; i<strlen(a); i++)
+    {
+        if(a[i]=='A')
+        a[i]='0';
+            else if(a[i]=='O')
+            a[i]='0';
+                else if(a[i]=='Y')
+                a[i]='0';
+                    else if(a[i]=='E')
+                    a[i]='0';
+                        else if(a[i]=='U')
+                        a[i]='0';
+                            else if(a[i]=='I')
+                            a[i]='0';
+                                else if(a[i]=='a')
+                                a[i]='0';
+                                    else if(a[i]=='o')
+                                    a[i]='0';
+                                        else if(a[i]=='y')
+                                        a[i]='0';
+                                            else if(a[i]=='e')
+                                            a[i]='0';
+                                                else if(a[i]=='u')
+                                                a[i]='0';
+                                                    if(a[i]=='i')
+                                                    a[i]='0';
+    }
+    
+    for(i=0; i<strlen(a); i++)
+    {
+        if(a[i]!='0')
         {
-               if(n==0)
-               {
-                       if(a[i]=='W')
-                       {
-                               n++;
-                       }
-                       else
-                       {
-                               ans[j]=a[i];
-                               j++;
-                       }
-                       if(i==strlen(a)-1 && a[i]=='W')
-                       {
-                               ans[j]=a[i];
-                               j++;
-                       }
-               }
-               else if(n==1)
-               {
-                       if(a[i]=='U')
-                       {
-                               n++;
-                       }
-                       else if(a[i]=='W')
-                       {
-                               ans[j]='W';
-                               j++;
-                               n=1;
-                       }
-                       else
-                       {
-                               ans[j]='W';
-                               j++;
-                               ans[j]=a[i];
-                               j++;
-                               n=0;
-                       }
-                       if(i==strlen(a)-1 && a[i]=='U')
-                       {
-                               ans[j]='W';
-                               j++;
-                               ans[j]='U';
-                               j++;
-                       }
-               }
-               else if(n==2)
-               {
-                       if(a[i]=='B')
-                       {
-                               n=0;
-                               ans[j]=' ';
-                               j++;
-                               if(j==1)
-                                      j--;
-                       }
-                       else
-                       {
-                               ans[j]='W';
-                               j++;
-                               ans[j]='U';
-                               j++;
-                               ans[j]=a[i];
-                               j++;
-                               n=0;
-                               if(a[i]=='W')
-                               {       
-                                      n=1;
-                                      j--;
-                               }
-                       }
- 
-               }
+            printf(".%c",a[i]);
         }
-        ans[j]='\0';
- 
-        printf("%s\n",ans);
-        return 0;
+    }
+
+    return 0;
 }
