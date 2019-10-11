6796bf6aae75b4fe1dc676582b74535802bf1ac6
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2019-10-11 10:54:53 +0530

    Add files via upload

--- jdoodle.c
+++ jdoodle.c
@@ -1,27 +1,23 @@
 #include<stdio.h>
 #include<string.h>
 
-int main() {
-    int i,n=0,p;
-    char a[100];
+int main()
+{
+    int i,j,l,n;
+    char s[100],a[5]="hello";
+    scanf("%s\n",&s);
+    l=strlen(s);
     
-    scanf("%s\n",&a);
-    n=strlen(a);
-    
-    for(i=0;i<n;i++)
+    for(i=0;i<l;i++)
     {
-        if(a[i]=='0' && a[i+1]=='0' && a[i+2]=='0' && a[i+3]=='0' && a[i+4]=='0' && a[i+5]=='0' && a[i+6]=='0')
+        if(s[i]==a[j])
         {
-            p=7;
-        }
-        else if(a[i]=='1' && a[i+1]=='1' && a[i+2]=='1' && a[i+3]=='1' && a[i+4]=='1' && a[i+5]=='1' && a[i+6]=='1')
-        {
-            p=7;
-            break;
+            j++;
+            n++;
         }
     }
     
-    if(p==7)
+    if(n==5)
     {
         printf("Yes\n");
     }
