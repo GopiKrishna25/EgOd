6d4ee471084f57ab01ccbd96a7a453522c031a45
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2019-11-20 14:53:33 +0530

    Add files via upload

--- jdoodle.c
+++ jdoodle.c
@@ -1,31 +1,28 @@
 #include<stdio.h>
-#include<string.h>
 
-int main() {
-    int i,j,l1,l2,cnt=0;
-    char s[100],t[100];
-
-    scanf("%s%s",&s,&t);
-    
-    l1=strlen(s);
-    l2=strlen(t);
-    
-    for(i=0,j=l2-1; i<l1,j>=0; i++,j--)
-    {
-        if(s[i]==t[j])
-        {
-            cnt++;
-        }
-    }
-    
-    if(cnt==l1)
-    {
-        printf("Yes\n");
-    }
+int year(int y)
+{
+    int a,b,c,d;
+    a=y%10;
+    b=(y/10)%10;
+    c=(y/100)%10;
+    d=(y/1000)%10;
+    if(a!=b&&a!=c&&a!=d&&b!=c&&b!=d&&c!=d)
+        return 1;
     else
-    {
-        printf("No\n");
-    }
+        return 0;
+}
 
+int main()
+{
+    int y,x,i;
+    scanf("%d",&y);
+    for(i=y+1;i<=9012;i++){
+       x=year(i);
+       if(x==1){
+        printf("%d\n",i);
+        break;
+       }
+    }
     return 0;
 }
