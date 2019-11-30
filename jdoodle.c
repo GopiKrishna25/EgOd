5504e6fea7e62ce4561fcba4ab7f3b3269087739
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2019-11-30 11:46:51 +0530

    Add files via upload

--- jdoodle.c
+++ jdoodle.c
@@ -2,24 +2,39 @@
 
 int main() 
 {
-    int n,i;
-    double sum=0.0;
-    scanf("%d\n",&n);
-    int a[n];
+    int i,j,a[5],cnt=0,tmp=0;
     
-    for(i=0;i<n;i++)
+    for(i=0;i<4;i++)
     {
-        scanf("%d\n",&a[i]);
+        scanf("%d\n",&a[i]);    
     }
     
-    for(i=0;i<n;i++)
+    
+    for(i=0; i<4; ++i)
     {
-        sum+=a[i];
+        for(j=i+1; j<4; ++j)
+        {
+            if(a[i] >a[j])
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
     }
     
-    sum=sum/n;
     
-    printf("%.12lf\n",sum);
+    for(i=0;i<3;i++)
+    {
+        if(a[i]==a[i+1])
+        {
+            cnt++;
+        }
+    }
+    
+    printf("%d\n",cnt);
     
     return 0;
+    
+    
 }
