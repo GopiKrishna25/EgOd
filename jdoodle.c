fba694baa7c34356c6a038b8ab1ac5aa5e071666
GopiKrishna25 <54054243+GopiKrishna25@users.noreply.github.com>
2020-02-14 19:15:31 +0530

    Add files via upload

--- jdoodle.c
+++ jdoodle.c
@@ -1,35 +1,34 @@
-#include<stdio.h>
-
-int main()
+#include<stdio.h> 
+int main() 
 {
-    int n,i,cnt=0;
-    scanf("%d",&n);
-    int a[n];
+    int s,i,j;
+    int a[25],n,sum; 
+    
+    scanf("%d %d",&n,&sum);
     
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d ",&a[i]);
     }
     
-    int maxx=a[0];
-    int minn=a[0];
-    
-    for(i=0;i<n;i++)
-    {
-        if(a[i]>maxx)
-        {
-            maxx=a[i];
-            cnt++;
-        }
-        if(a[i]<minn)
-        {
-           minn=a[i];
-           cnt++;
-        }
-    }
-    
-    
-    printf("%d\n",cnt);
-
+    for(i=0;i<n;i++) 
+    { 
+        s = a[i]; 
+        for (j=i+1;j<=n;j++) 
+        { 
+            if (s == sum) 
+            { 
+                printf ("Sum found between indexes %d and %d", i, j-1); 
+                return 1; 
+            } 
+            if (s > sum || j == n) 
+                break; 
+           s = s + a[j]; 
+        } 
+    } 
+  
+    printf("No subarray found"); 
     return 0;
-}
+    
+    return 0; 
+} 
